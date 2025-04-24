ef8fb0c97f3431890e96cec5ce247429f885bbde
Özgürhan Polat <ozgurhan.45@gmail.com>
2025-04-24 09:39:58 +0300

    product is added

--- src/main/java/org/softwaredev/sdweek6/product/application/ProductService.java
+++ src/main/java/org/softwaredev/sdweek6/product/application/ProductService.java
@@ -6,6 +6,7 @@ import org.softwaredev.sdweek6.category.domain.entity.Category;
 import org.softwaredev.sdweek6.product.domain.entity.Product;
 import org.softwaredev.sdweek6.product.domain.http.request.ProductRequest;
 import org.softwaredev.sdweek6.product.exceptions.CategoryNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +16,18 @@ public class ProductService {
 
   private final CategoryService categoryService;
 
-  public ResponseEntity<Product> createProduct(ProductRequest productRequest) throws CategoryNotFoundException {
+  public ResponseEntity<Product> createProduct(ProductRequest productRequest)
+      throws CategoryNotFoundException {
 
     Category category = categoryService.findCategoryById(productRequest.categoryId());
 
-    Product product = Product.builder()
+    Product product =
+        Product.builder()
             .productName(productRequest.productName())
+            .category(category)
             .price(productRequest.price())
             .build();
 
-
-    return null;
+    return new ResponseEntity<>(product, HttpStatus.CREATED);
   }
 }
